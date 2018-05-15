15e97148c0aa4ef4b0dd02f646665c55e8b820df
Adam Sitnik <adam.sitnik@gmail.com>
2018-05-15 22:17:10 +0200

    Harness (#1)
    * harnessing AllReady
    
    * copy of the AspNet-GenerateStore.ps1 script
    
    * fix the path - the current directory is folder with AllReady.csproj
    
    * move the folder where R2R images are stored closer to root to avoid too long path exceptions..
    
    * rename remaining "JitBench" to "Scenarios"
    
    * I forgot...
    
    * remove all store files (moved to perf repo)

--- AllReadyApp/Web-App/AllReady/Program.cs
+++ AllReadyApp/Web-App/AllReady/Program.cs
@@ -1,20 +1,45 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Scenarios;
+using System;
 
 namespace AllReady
 {
     public class Program
     {
+        const string Url = "http://localhost:5000";
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var scenarios = ScenariosHelper.Start();
+
+            var config = new ConfigurationBuilder()
+               .AddCommandLine(Array.Empty<string>())
+               .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+               .Build();
+
+            BuildWebHost(args, config).Start();
+
+            scenarios.LogStartup();
+
+            scenarios.PerformHttpRequests(Url, args, threshholds: new int[] { 100, 250, 500, 750, 1000, 1500, 2000 });
+
+            scenarios.VerifyLibraryLocation();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args, IConfigurationRoot configRoot) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(configRoot)
                 .ConfigureAppConfiguration((ctx, config) => config.SetBasePath(ctx.HostingEnvironment.ContentRootPath).AddJsonFile("version.json"))
                 .UseStartup<Startup>()
+                .UseUrls(Url)
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole();
+                    factory.AddFilter((category, level) => level >= LogLevel.Warning);
+                })
                 .Build();
     }
 }
